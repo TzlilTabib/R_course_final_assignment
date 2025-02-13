2a83aadfcb500ccfaeacfd8b64cd6f30e7b1707d
Tzlil Tabib <tzliltabib@gmail.com>
2025-02-13 16:21:30 +0200

    4th commit - adding more figures
    Adding more figures to the script, also using ggdist

--- presenting_raw_data.R
+++ presenting_raw_data.R
@@ -8,7 +8,7 @@ df <- read.csv("./raw_data_wide.csv")
 figure_1 <- ggplot(df, aes(x = age, fill = gender)) +
   geom_histogram(bins = 5, alpha = 0.8) +
   scale_fill_manual(values = c("male" = "#93e6d4", "female" = "#f57a9f")) +
-  labs(title   = "Figure 1: Age by Gender",
+  labs(title   = "Age by Gender",
        caption = "Note: N = 22",
        x       = "age",
        y       = "count",
@@ -19,29 +19,63 @@ figure_1 <- ggplot(df, aes(x = age, fill = gender)) +
 
 ggsave("./figure_1.png", figure_1, bg = "white", width = 8, height = 6)
 
+# Government support and education level
+figure_2 <- ggplot(df, aes(x = education, fill = government_support)) +
+  geom_bar(stat = "count", alpha = 0.7) +
+  scale_y_continuous(breaks = seq(0, 10, 1)) +  
+  labs(title   = "Education level by government support (categorical)",
+       caption = "Government support coded as <= 40 (support in Netanyahu) and >=60 as support in Benet-Lapid",
+       x       = "Education level",
+       y       = "count",
+       fill    = "Government Support") +
+  scale_fill_manual(values = c("Benet-Lapid" = "blue", "Netanyahu" = "red")) +
+  theme_minimal() +
+  theme(
+    plot.caption = element_text(hjust = 0, face = "italic"))
+
+ggsave("./figure_2.png", figure_2, bg = "white", width = 8, height = 6)
+
 # Political wing affiliation by coalition/opposition support
-figure_2 <- ggplot(df, aes(x = coalition_opposition, y = political_camp_raw, fill = coalition_opposition)) +
+figure_3 <- ggplot(df, aes(x = coalition_opposition, y = political_camp_raw, fill = coalition_opposition)) +
   geom_point(size = 2) +
   geom_boxplot(alpha = 0.6) +
   labs(title    = "Political wing affiliation by coalition/opposition support",
-       caption = "The scale of political wing affiliation was captioned in the survey: 
+       caption  = "The scale of political wing affiliation was captioned in the survey: 
        * lower end (0 on the y axis) as right wing
        * upper end (100 on the y axis) as left wing",
        x        = "Dichotomic coalition / opposition support", 
        y        = "Political wing affiliation rating",
        fill     = "Coalition or opposition support") +
+  scale_fill_manual(values = c("opposition" = "blue", "coalition" = "red")) +
   theme_minimal() +
   theme(
     plot.caption = element_text(hjust = 0, face = "italic"))
   
-ggsave("./figure_2.png", figure_2, bg = "white", width = 8, height = 6)
-  
+ggsave("./figure_3.png", figure_3, bg = "white", width = 8, height = 6)
 
+## Multivariate plots
+# Age by political affiliation and gender
+figure_4 <- ggplot(df, aes(x = age, y = gender, fill = political_camp)) +
+  geom_density_ridges(alpha = 0.6) +
+  labs(title = "Age density distribution by political affiliation and gender",
+       x     = "Age",
+       y     = "Gender",
+       fill  = "Political wing affiliation") +
+  scale_fill_manual(values = c("right" = "red", "left" = "blue")) +
+  theme_minimal()
 
+ggsave("./figure_4.png", figure_4, bg = "white", width = 8, height = 6)
 
+# Political involvement by government support and political camp
+figure_5 <- ggplot(df, aes(x = political_involvement, y = government_support, fill = political_camp)) +
+  stat_halfeye(adjust = 0.5, justification = -0.2, .width = 0, alpha = 0.7) +
+  geom_boxplot(width = 0.2, outlier.shape = NA, alpha = 0.7) +
+  geom_jitter(position = position_nudge(y = -0.2), alpha = 0.7) +
+  theme_minimal() +
+  labs(title = "Political involvement by government support and political camp",
+       x     = "Political involvement",
+       y     = "Government support",
+       fill  = "Political camp") +
+  scale_fill_manual(values = c("left" = "blue", "right" = "red"))
 
-# Multivariate plots
-
-
-
-
+ggsave("./figure_5.png", figure_5, bg = "white", width = 8, height = 6)
